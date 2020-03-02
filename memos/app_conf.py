66a34f3d95e0ca10838bb6278697f585c0da299e
Lude <new@example.com>
2020-03-03 08:27:40 +0900

    [update] update tests

--- memos/app_conf.py
+++ memos/app_conf.py
@@ -1,5 +1,13 @@
 import sys
-import configperser
+import configparser
+
+CONF_PATH = "../conf/app.conf"
+config_ini = configparser.ConfigParser()
+config_ini.read(CONF_PATH, encoding="utf-8")
 
 def get_config(key: str):
-    pass
+    re_property = None
+    if key in "editor":
+        re_property = config_ini.get("env", "editor")
+    return re_property
+    
--- tests/test_app_conf.py
+++ tests/test_app_conf.py
@@ -2,11 +2,20 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-
 import unittest
 import pytest
+import configparser
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '../memos/'))
 
+import app_conf
+
+CONF_PATH = "../conf/app.conf"
+config_ini = configparser.ConfigParser()
+config_ini.read(CONF_PATH, encoding="utf-8")
 
 def test_conf_get_001():
-    pass
+    conf_001 = config_ini.get("env", "editor")
+    assert app_conf.get_config("editor") == conf_001
+    
+
