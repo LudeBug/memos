b7b37f036fb676b986748c1a8c67cb26c1f308c3
Lude <new>
2020-03-09 09:33:52 +0900

    [update] add app_conf

--- memos/app_conf.py
+++ memos/app_conf.py
@@ -8,9 +8,16 @@ CONF_PATH = "conf/app.conf"
 config_ini = configparser.ConfigParser()
 config_ini.read(CONF_PATH, encoding="utf-8")
 
+
 def get_config(key: str):
     re_property = None
     if key in "editor":
         re_property = config_ini.get("env", "editor")
+    elif key in "db_csv_path":
+        re_property = config_ini.get("env", "db_csv_path")
+    elif key in "range":
+        re_property = config_ini.get("env", "range")
+    elif key in "log_level":
+        re_property = config_ini.get("env", "log_level")
     return re_property
     
--- tests/test_app_conf.py
+++ tests/test_app_conf.py
@@ -17,5 +17,18 @@ config_ini.read(CONF_PATH, encoding="utf-8")
 def test_conf_get_001():
     conf_001 = config_ini.get("env", "editor")
     assert app_conf.get_config("editor") == conf_001
-    
 
+
+def test_conf_get_002():
+    conf_001 = config_ini.get("env", "db_csv_path")
+    assert app_conf.get_config("db_csv_path") == conf_001
+
+
+def test_conf_get_003():
+    conf_001 = config_ini.get("env", "range")
+    assert app_conf.get_config("range") == conf_001
+
+
+def test_conf_get_004():
+    conf_001 = config_ini.get("env", "log_level")
+    assert app_conf.get_config("log_level") == conf_001
